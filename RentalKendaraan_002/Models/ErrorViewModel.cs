9b5182fe435e047339706cf3579242adfaaf90d2
NaufalHaryanda <naufalharyanda10@gmail.com>
2020-12-05 19:12:17 +0700

    Add project files.

---
+++ RentalKendaraan_002/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RentalKendaraan_002.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
